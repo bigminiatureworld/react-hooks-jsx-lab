1b1455bc4f76a240bb2024882c1b0562f88fada7
Adam Weaver <adamweaver90@gmail.com>
2022-10-11 18:20:28 -0400

    commit tree dd1a58a10584027c4cbe6570e212b9d7dd6c72a1

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,15 @@
 import React from "react";
 import { name, city } from "../data/data.js";
 
-function Home() {
-  // update the JSX being returned!
-  return <div>Home</div>;
+// const divStyle = {
+//   color: 'firebrick'
+// }
+function Home(){
+  return (
+      <div id="home">
+          <h1 style={{ color: 'firebrick' }}>Adam is a Web Developer from Haddonfield</h1>
+      </div>
+  )
 }
 
 export default Home;
--- src/index.js
+++ src/index.js
@@ -4,3 +4,12 @@ import "./index.css";
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+
+
+
+
+
+
+
+
